13b58793d2784bbcb318484d954805dfce4d8d8d
Tyler Fox <tyler@blur.com>
2025-09-16 14:30:07 -0700

    morefix

--- get_version.py
+++ get_version.py
@@ -5,34 +5,28 @@ import sys
 VERSION_FILE = "version.txt"
 
 
-def main(root):
+def main(srcroot):
     # If version.txt already exists, use it
-    ver_file = os.path.join(root, VERSION_FILE)
-
-    if os.path.exists(ver_file):
-        with open(ver_file) as f:
+    src_file = os.path.join(srcroot, VERSION_FILE)
+    if os.path.exists(src_file):
+        with open(src_file) as f:
             v = f.read().strip()
-        print(v)
-        return
-
-    # Otherwise, try git describe
-    try:
-        # fmt: off
-        v = subprocess.check_output(
-            ["git", "describe", "--tags", "--always", "--match", "v[0-9]*", "--abbrev=0"],
-            text=True,
-        ).strip()
-        # fmt: on
-    except Exception:
-        v = "0.0.1"  # fallback if not in a git repo
-
-    # Write version.txt so future builds can reuse it
-    with open(ver_file, "w") as f:
-        f.write(v)
+    else:
+        # Otherwise, try git describe
+        try:
+            # fmt: off
+            v = subprocess.check_output(
+                ["git", "describe", "--tags", "--always", "--match", "v[0-9]*", "--abbrev=0"],
+                text=True,
+            ).strip()
+            # fmt: on
+        except Exception:
+            v = "0.0.1"  # fallback if not in a git repo
 
     print(v)
 
 
 if __name__ == "__main__":
-    root = sys.argv[1]
-    sys.exit(main(root))
+    srcroot = sys.argv[1]
+    buildroot = sys.argv[2]
+    sys.exit(main(srcroot))
