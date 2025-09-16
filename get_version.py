8b0c1c04c2faf6711b780f9ec38368a7487fb7a5
Tyler Fox <tyler@blur.com>
2025-09-16 14:34:50 -0700

    pass git path

--- get_version.py
+++ get_version.py
@@ -5,7 +5,7 @@ import sys
 VERSION_FILE = "version.txt"
 
 
-def main(srcroot):
+def main(srcroot, gitpath):
     # If version.txt already exists, use it
     src_file = os.path.join(srcroot, VERSION_FILE)
     if os.path.exists(src_file):
@@ -16,7 +16,7 @@ def main(srcroot):
         try:
             # fmt: off
             v = subprocess.check_output(
-                ["git", "describe", "--tags", "--always", "--match", "v[0-9]*", "--abbrev=0"],
+                [gitpath, "describe", "--tags", "--always", "--match", "v[0-9]*", "--abbrev=0"],
                 text=True,
             ).strip()
             # fmt: on
@@ -25,7 +25,7 @@ def main(srcroot):
 
     print(v)
 
-
 if __name__ == "__main__":
     srcroot = sys.argv[1]
-    sys.exit(main(srcroot))
+    gitpath = sys.argv[2]
+    sys.exit(main(srcroot, gitpath))
